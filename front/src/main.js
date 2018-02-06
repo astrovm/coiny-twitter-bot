98b4564aa6f9ed45cbac96ead798025f867de224
astro <astro@astrolince.com>
2018-02-06 12:42:44 -0300

    buefy custom init

--- front/src/main.js
+++ front/src/main.js
@@ -4,7 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Buefy from 'buefy'
-import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
 Vue.use(Buefy)
