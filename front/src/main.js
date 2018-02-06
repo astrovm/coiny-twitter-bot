5a8152f33459e8ec3a1cd208568c8ab7f4c284e3
astro <astro@astrolince.com>
2018-02-06 12:33:45 -0300

    add buefy

--- front/src/main.js
+++ front/src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
+Vue.use(Buefy)
 
 /* eslint-disable no-new */
 new Vue({
