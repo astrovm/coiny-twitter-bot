657e2b9178875aa3d5b9ec21ad16397c2d7d9b0f
astro <astro@astrolince.com>
2018-02-16 18:48:40 -0300

    1

--- front/src/api/index.js
+++
@@ -1,9 +0,0 @@
-const URL = 'https://coiny.sh/api/v1/'
-
-exports.getFees = () => {
-  return fetch(URL + 'tx/fee/').then(res => res.json())
-}
-
-exports.getPrice = () => {
-  return fetch(URL + 'price/btcusd/').then(res => res.json())
-}
