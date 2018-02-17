e6bd0e255a3298aaaab50f582f2971622328a3ac
astro <astro@astrolince.com>
2018-02-17 04:18:35 -0300

    lint

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ module.exports = {
     /*
      ** Run ESLINT on save
      */
-    extend (config, ctx) {
+    extend(config, ctx) {
       if (ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import Koa from 'koa'
 import { Nuxt, Builder } from 'nuxt'
 
-async function start () {
+async function start() {
   const app = new Koa()
   const host = process.env.HOST || '127.0.0.1'
   const port = process.env.PORT || 3000
