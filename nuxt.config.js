80f0b8dd6eb984a8fb8be3999dfb85e63bb93025
astro <astro@astrolince.com>
2018-02-18 06:31:13 -0300

    jkjk

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+      { hid: 'description', name: 'description', content: 'Bitcoin fee estimates and market info.' },
       { name: 'apple-mobile-web-app-title', content: 'Coiny' },
       { name: 'application-name', content: 'Coiny' },
       { name: 'msapplication-TileColor', content: '#2d3436' },
