1ab786cc0969c6bdd3535e4592147eac815ac3c7
astrolince <astro@astrolince.com>
2018-08-20 05:33:05 -0300

    add blockchain size

--- server/blockchain.js
+++ server/blockchain.js
@@ -4,27 +4,29 @@
 const trae = require('trae')
 const schedule = require('node-schedule')
 
-// request smartbit api totals
-const getTotals = async () => {
+// request smartbit api
+const getBlockchain = async () => {
   try {
-    const res = await trae.get('https://api.smartbit.com.au/v1/blockchain/totals')
-    totals = res.data.totals
-    return totals
+    const resTotals = await trae.get('https://api.smartbit.com.au/v1/blockchain/totals')
+    const resBlockchainSize = await trae.get('https://api.smartbit.com.au/v1/blockchain/chart/block-size-total?from=2018-8-15')
+    totals = resTotals.data.totals
+    blockchainSize = resBlockchainSize.data.chart.data.slice(-1)[0].y/1000000000
   } catch (err) {
     console.error(err)
-    if (totals) return totals
     return err
   }
 }
 
-// init totals data
+// init data
 let totals = {}
-getTotals()
+let blockchainSize = {}
+getBlockchain()
 
-// get smartbit totals every x minutes job
+// get smartbit data every x minutes job
 schedule.scheduleJob('9-59/10 * * * *', () => {
-  getTotals()
+  getBlockchain()
 })
 
-// export totals function
-module.exports = () => totals
+// export data
+exports.totals = totals
+exports.blockchainSize = blockchainSize
