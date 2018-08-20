ba36b0c7aba202d41c3018230ca9afe361888a8f
astrolince <astro@astrolince.com>
2018-08-20 07:08:56 -0300

    test

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const buildText = async (fees) => {
   if (fees[504] < fees[144]) text = text + `\n3 days ${fees[504]} sat/B ($${(fees[504] * usd).toFixed(2)})`
   if (fees[1008] < fees[504]) text = text + `\n7 days ${fees[1008]} sat/B ($${(fees[1008] * usd).toFixed(2)})`
   text = text + `\n\nheight ${block.block_count-1}`
-  text = text + `\nblockchain size ${(size).toFixed(2)} GB`
+  text = text + `\nblockchain size ${Number(size).toFixed(2)} GB`
   text = text + `\nprice $${price()}`
   return text
 }
