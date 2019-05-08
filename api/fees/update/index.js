e100144dd2ddcbea553007a485567942431a7d3b
astrolince <astro@astrolince.com>
2019-05-08 05:01:00 -0300

    updating fees api

---
+++ api/fees/update/index.js
@@ -0,0 +1,89 @@
+// require libs
+const trae = require('trae')
+
+// require and config db
+const redis = require('redis');
+const redisPort = process.env.REDIS_PORT;
+const redisHost = process.env.REDIS_HOST;
+const redisPass = process.env.REDIS_PASS;
+const redisClient = redis.createClient(redisPort, redisHost);
+redisClient.auth(redisPass);
+
+redisClient.on('error', (err) => {
+    console.error('Error ' + err);
+});
+
+// request bitgo api fees
+const getFees = async () => {
+    try {
+        const res = await trae.get('https://www.bitgo.com/api/v1/tx/fee')
+        const newFees = await sortFees(res.data.feeByBlockTarget)
+        fees = newFees
+        return newFees
+    } catch (err) {
+        console.error(err)
+        if (fees) return fees
+        return err
+    }
+}
+
+// sort fees object
+const sortFees = (req) => {
+    const feesSorted = Object.keys(req).sort((a, b) => req[b] - req[a]) // sort fee numbers
+    const blocksSorted = Object.keys(req).sort((a, b) => a - b) // sort block target numbers
+    // recreate fees object by matching sorted blocks with sorted fees
+    let res = {}
+    for (let i in feesSorted) {
+        res[blocksSorted[i]] = Math.ceil(req[feesSorted[i]] / 1000)
+    }
+    return res
+}
+
+// select fee for specific block target
+const feeFor = async (blocks) => {
+    const feeData = (Object.keys(fees).length === 0) ? await getFees() : fees  // if fees obj is empty fill it
+    if (Object.keys(feeData).length === 0) return { 0: 0 }
+    const feeDataSorted = Object.keys(feeData).sort((a, b) => b - a) // sort block targets from highest to lowest
+    const minBlock = parseInt(feeDataSorted.slice(-1)[0])
+    let res = {}
+    for (let b in blocks) {
+        const target = (blocks[b] < minBlock) ? minBlock : blocks[b]
+        for (let i in feeDataSorted) {
+            if (target >= feeDataSorted[i]) {
+                res[blocks[b]] = feeData[feeDataSorted[i]]
+                break
+            }
+        }
+    }
+    return res
+}
+
+// export api
+module.exports = (req, res) => {
+    // check last time updated
+    redisClient.get('blocks:time', async (err, reply) => {
+        if (err) {
+            console.error('Error ' + err);
+        }
+
+        const ONE_HOUR = 60 * 60 * 1000;
+        const currentTime = Date.now();
+        const keyTime = ((reply == null) ? (currentTime - ONE_HOUR) : reply);
+
+        // if last time >= one hour, update it now
+        if ((currentTime - keyTime) >= ONE_HOUR) {
+            const blocks = JSON.stringify(await getBlocks());
+            const currentTime = Date.now();
+
+            redisClient.set('blocks', blocks, (err, reply) => {
+                console.log(err, reply)
+                redisClient.set('blocks:time', currentTime, (err, reply) => {
+                    console.log(err, reply)
+                    res.end('Updated ' + blocks);
+                });
+            });
+        } else {
+            res.end('Already updated ' + req.url);
+        }
+    });
+};
