fb8117e9368aa0d360ae2677d9e579de0d69a405
astrolince <astro@astrolince.com>
2019-05-09 23:18:13 -0300

    promisifying blocks api

--- api/blocks/index.js
+++ api/blocks/index.js
@@ -1,3 +1,6 @@
+// require promisify
+const { promisify } = require('util');
+
 // require and config db libs
 const redis = require('redis');
 const redisPort = process.env.REDIS_PORT;
@@ -5,19 +8,30 @@ const redisHost = process.env.REDIS_HOST;
 const redisPass = process.env.REDIS_PASS;
 const redisClient = redis.createClient(redisPort, redisHost);
 redisClient.auth(redisPass);
-
-redisClient.on('error', function (err) {
+redisClient.on('error', (err) => {
     console.error('Error ' + err);
 });
+const redisGet = promisify(redisClient.get).bind(redisClient);
 
 // export api
-module.exports = (req, res) => {
-    redisClient.get('blocks', (err, reply) => {
+module.exports = async (req, res) => {
+    try {
+        const redisReplyBlocksGet = await redisGet('blocks');
+
         let respond = {};
-        respond.blocks = reply;
-        respond.error = err;
+        respond.blocks = redisReplyBlocksGet;
+        respond.error = false;
         respond.path = req.url;
 
         res.end(JSON.stringify(respond));
-    });
+    } catch (err) {
+        console.error(err)
+
+        let respond = {};
+        respond.blocks = null;
+        respond.error = true;
+        respond.path = req.url;
+
+        res.end(JSON.stringify(respond));
+    };
 };
