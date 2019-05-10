e2982888176422a10aebf00830b347fe83270aac
astrolince <astro@astrolince.com>
2019-05-09 23:22:06 -0300

    add promisify

--- api/blocks/update/index.js
+++ api/blocks/update/index.js
@@ -1,5 +1,6 @@
 // require libs
 const trae = require('trae')
+const { promisify } = require('util');
 
 // require and config db
 const redis = require('redis');
@@ -8,10 +9,11 @@ const redisHost = process.env.REDIS_HOST;
 const redisPass = process.env.REDIS_PASS;
 const redisClient = redis.createClient(redisPort, redisHost);
 redisClient.auth(redisPass);
-
 redisClient.on('error', (err) => {
     console.error('Error ' + err);
 });
+const redisGet = promisify(redisClient.get).bind(redisClient);
+const redisSet = promisify(redisClient.set).bind(redisClient);
 
 // request smartbit api
 const getBlocks = async () => {
