926a9875d2d893b6f6756adc6d9c30ed9c1257b0
astrolince <astro@astrolince.com>
2020-01-23 03:38:02 -0300

    add satoshitango, cryptomkt, bitex

--- api/argentina/update/index.js
+++ api/argentina/update/index.js
@@ -8,10 +8,16 @@ const getPrices = async () => {
     const ripio = await trae.get('https://ripio.com/api/v1/rates/')
     const bitso = await trae.get('https://api.bitso.com/v3/ticker/?book=btc_ars')
     const argenbtc = await trae.get('https://argenbtc.com/public/cotizacion_js.php')
+    const satoshitango = await trae.get('https://api.satoshitango.com/v3/ticker/ARS')
+    const cryptomkt = await trae.get('https://api.cryptomkt.com/v1/ticker?market=BTCARS')
+    const bitex = await trae.get('https://bitex.la/api/tickers/btc_ars')
 
     const ripioPrices = ripio.data.rates
     const bitsoPrices = bitso.data.payload
     const argenbtcPrices = JSON.parse(argenbtc.data)
+    const satoshitangoPrices = satoshitango.data.data.ticker
+    const cryptomktPrices = cryptomkt.data.data[0]
+    const bitexPrices = JSON.parse(bitex.data).data
 
     const prices = {
       BTC_ARS: {
@@ -29,6 +35,21 @@ const getPrices = async () => {
           bid: Number(argenbtcPrices.precio_venta), // spread fee
           ask: Number(argenbtcPrices.precio_compra), // spread fee
           networkfee: 0.00005000 // https://argenbtc.com/SolicitarRetirosBTC
+        },
+        satoshitango: {
+          bid: Number(satoshitangoPrices.BTC.bid) * 0.99, // 1% fee
+          ask: Number(satoshitangoPrices.BTC.ask) * 1.01, // 1% fee
+          networkfee: 0.00030000 // https://www.satoshitango.com/help
+        },
+        cryptomkt: {
+          bid: Number(cryptomktPrices.bid) * 0.985, // 1.5% fee
+          ask: Number(cryptomktPrices.ask) * 1.015, // 1.5% fee
+          networkfee: 0.00050000 // https://www.cryptomkt.com/en/fees
+        },
+        bitex: {
+          bid: Number(bitexPrices.attributes.bid) * 0.989 * 0.9975, // 1.1% + 0.25% fee
+          ask: Number(bitexPrices.attributes.ask) * 1.011 * 1.0025, // 1.1% + 0.25% fee
+          networkfee: 0.0 
         }
       }
     }
